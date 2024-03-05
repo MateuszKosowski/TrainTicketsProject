1d1b68b6736f5ec15bf3891c527ff2d93d0265ac
Mateusz Kosowski <251558@edu.p.lodz.pl>
2024-03-05 10:43:12 +0100

    Math helper factorial

--- kosowski/introduction/program/src/main.cpp
+++ kosowski/introduction/program/src/main.cpp
@@ -3,8 +3,11 @@
 #include <iostream>
 using namespace std;
 
+#include "math_helpers.h"
+
 int main()
 {
 	cout << "Hello, World!" << endl;
+	cout << "factorial(5) = " << factorial(5) << endl;
 	return 0;
 }
