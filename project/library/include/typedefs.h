b1d38c11db91d6f30cbe66fd2173d59ed697c25c
Mateusz Kosowski <251558@edu.p.lodz.pl>
2024-05-31 15:29:18 +0200

    Add typedefs.h

---
+++ project/library/include/typedefs.h
@@ -0,0 +1,16 @@
+//
+// Created by student on 31.05.24.
+//
+
+#ifndef TRAINTICKETS_TYPEDEFS_H
+#define TRAINTICKETS_TYPEDEFS_H
+
+#include <memory>
+#include <string>
+#include <vector>
+
+class Train;;
+
+typedef std::shared_ptr<Train> TrainPtr;
+
+#endif //TRAINTICKETS_TYPEDEFS_H
