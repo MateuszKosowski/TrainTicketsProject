ae1b005b24cf08edb1bed30d7510e56f95b9699c
Mateusz Kosowski <251558@edu.p.lodz.pl>
2024-06-08 00:19:24 +0200

    Add dosc for Station and Route classes

--- project/library/include/model/Route.h
+++ project/library/include/model/Route.h
@@ -7,39 +7,122 @@
 
 #include <typedefs.h>
 
+/**
+ * @class Route
+ * @brief Klasa reprezentująca trasę w systemie.
+ */
+
 class Route {
 private:
+    /**
+     * @brief Nazwa stacji początkowej trasy.
+     */
     std::string startStation;
+
+    /**
+     * @brief Nazwa stacji końcowej trasy.
+     */
     std::string endStation;
+
+    /**
+     * @brief ID trasy.
+     */
     std::string routeID;
+
+    /**
+     * @brief Długość trasy.
+     */
     int distance;
+
+    /**
+     * @brief Czas trwania trasy.
+     */
     int duration;
 
 public:
+
+    /**
+     * @brief Metoda zwracająca ID trasy.
+     * @return ID trasy.
+     */
     const std::string &getRouteId() const;
 
+
+    /**
+     * @brief Metoda ustawiająca ID trasy.
+     * @param routeId - ID trasy.
+     */
     void setRouteId(const std::string &routeId);
 
+
+    /**
+     * @brief Metoda zwracająca nazwę stacji początkowej trasy.
+     * @return Nazwa stacji początkowej trasy.
+     */
     const std::string &getStartStation() const;
 
+    /**
+     * @brief Metoda ustawiająca nazwę stacji początkowej trasy.
+     * @param startStationP - nazwa stacji początkowej trasy.
+     */
     void setStartStation(const std::string &startStationP);
 
+    /**
+     * @brief Metoda zwracająca nazwę stacji końcowej trasy.
+     * @return Nazwa stacji końcowej trasy.
+     */
     const std::string &getEndStation() const;
 
+
+    /**
+     * @brief Metoda ustawiająca nazwę stacji końcowej trasy.
+     * @param endStationP - nazwa stacji końcowej trasy.
+     */
     void setEndStation(const std::string &endStationP);
 
+    /**
+     * @brief Metoda zwracająca długość trasy.
+     * @return Długość trasy.
+     */
     int getDistance() const;
 
+    /**
+     * @brief Metoda ustawiająca długość trasy.
+     * @param distanceP - długość trasy.
+     */
     void setDistance(int distanceP);
 
+    /**
+     * @brief Metoda zwracająca czas trwania trasy.
+     * @return Czas trwania trasy.
+     */
     int getDuration() const;
 
+    /**
+     * @brief Metoda ustawiająca czas trwania trasy.
+     * @param durationP - czas trwania trasy.
+     */
     void setDuration(int durationP);
 
+    /**
+     * @brief Metoda zwracająca informacje o trasie.
+     * @return Informacje o trasie.
+     */
     std::string getInfo() const;
 
+    /**
+     * @brief Konstruktor klasy Route.
+     * @param startStation - nazwa stacji początkowej trasy.
+     * @param endStation - nazwa stacji końcowej trasy.
+     * @param routeID - ID trasy.
+     * @param distance - długość trasy.
+     * @param duration - czas trwania trasy.
+     */
     Route(const std::string &startStation, const std::string &endStation,const std::string &routeID, int distance, int duration);
 
+    /**
+     * @brief Destruktor klasy Route.
+     */
     ~Route();
 
 };
--- project/library/include/model/Station.h
+++ project/library/include/model/Station.h
@@ -7,27 +7,77 @@
 
 #include "typedefs.h"
 
+/**
+ * @class Station
+ * @brief Klasa reprezentująca stację w systemie.
+ */
 class Station {
 private:
+
+    /**
+     * @brief Nazwa stacji.
+     */
     std::string name;
+
+    /**
+     * @brief Wektor przechowujący trasy przechodzące przez tą stację.
+     */
     std::vector<RoutePtr> routes;
+
 public:
+    /**
+     * @brief Metoda zwracająca nazwę stacji.
+     * @return Nazwa stacji.
+     */
     const std::string &getName() const;
 
+    /**
+     * @brief Metoda ustawiająca nazwę stacji.
+     * @param nameP - nazwa stacji.
+     */
     void setName(const std::string &nameP);
 
+    /**
+     * @brief Metoda zwracająca trasy przechodzące przez stację.
+     * @return Trasy przechodzące przez stację.
+     */
     const std::vector<RoutePtr> &getRoutes() const;
 
+    /**
+     * @brief Metoda zwracająca trasę o podanym ID.
+     * @param routeID - ID trasy.
+     * @return Trasa o podanym ID.
+     */
     RoutePtr getRoute(const std::string &routeID) const;
 
+    /**
+     * @brief Metoda dodająca trasę do stacji.
+     * @param route - trasa.
+     */
     void addRoute(const RoutePtr &route);
 
+    /**
+     * @brief Metoda usuwająca trasę ze stacji.
+     * @param route - trasa.
+     */
     void deleteRoute(const RoutePtr &route);
 
+    /**
+     * @brief Metoda zwracająca informacje o stacji.
+     * @return Informacje o stacji.
+     */
     std::string getInfo() const;
 
+    /**
+     * @brief Konstruktor klasy Station.
+     * @param name - nazwa stacji.
+     */
     Station(const std::string &name);
 
+
+    /**
+     * @brief Destruktor klasy Station.
+     */
     ~Station();
 };
 
