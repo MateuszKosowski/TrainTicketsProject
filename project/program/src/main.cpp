d3a5fe8096a343ec0b7836837b47ae091aca35df
Mateusz Kosowski <251558@edu.p.lodz.pl>
2024-06-07 16:43:50 +0200

    Add part of submenu

--- project/program/src/main.cpp
+++ project/program/src/main.cpp
@@ -8,14 +8,14 @@
 
 using namespace std;
 
+StationManagerPtr stationManager = std::make_shared<StationManager>();
+TrainManagerPtr trainManager = std::make_shared<TrainManager>();
+ClientManagerPtr clientManager = std::make_shared<ClientManager>();
+TicketManagerPtr ticketManager = std::make_shared<TicketManager>();
+
 int main()
 {
-    StationManagerPtr stationManager = std::make_shared<StationManager>();
-    TrainManagerPtr trainManager = std::make_shared<TrainManager>();
-    ClientManagerPtr clientManager = std::make_shared<ClientManager>();
-    TicketManagerPtr ticketManager = std::make_shared<TicketManager>();
-
-    char choice;
+    char choice, choice2;
     do{
         system("clear");
         cout << "--- Train Tickets ---\n";
@@ -28,6 +28,75 @@ int main()
         cout << "----------------------\n";
 
         cin >> choice;
+
+        switch(choice){
+            case '1':
+                do{
+                    cout << "--- Menager Stacji ---\n";
+                    cout << "Dostepne akcje:\n";
+                    cout << "1. Utworz stacje\n";
+                    cout << "2. Usun stacje\n";
+                    cout << "3. Utworz i dodaj trase do stacji\n";
+                    cout << "4. Wyswietl raport wszystkich stacji\n";
+                    cout << "5. Wyswietl raport wybranej stacji\n";
+                    cout << "6. Cofnij sie\n";
+                    cout << "----------------------\n";
+
+                    cin >> choice2;
+
+                } while (choice2 != '6');
+                break;
+
+            case '2':
+                do{
+                    cout << "--- Menager Pociagow ---\n";
+                    cout << "Dostepne akcje:\n";
+                    cout << "1. Utworz pociag\n";
+                    cout << "2. Usun pociag\n";
+                    cout << "3. Wyswietl raport wszystkich pociagow\n";
+                    cout << "4. Wyswietl raport wybranego pociagu\n";
+                    cout << "5. Cofnij sie\n";
+                    cout << "----------------------\n";
+
+                    cin >> choice2;
+
+                } while (choice2 != '5');
+                break;
+
+            case '3':
+                do{
+                    cout << "--- Menager Klientow ---\n";
+                    cout << "Dostepne akcje:\n";
+                    cout << "PRACE DEVELOPERSKIE WCIAZ TRWAJA\n";
+                    cout << "1. Cofnij sie\n";
+                    cout << "----------------------\n";
+
+                    cin >> choice2;
+
+                } while (choice2 != '1');
+                break;
+
+            case '4':
+                do{
+                    cout << "--- Menager Biletow ---\n";
+                    cout << "Dostepne akcje:\n";
+                    cout << "PRACE DEVELOPERSKIE WCIAZ TRWAJA\n";
+                    cout << "1. Cofnij sie\n";
+                    cout << "----------------------\n";
+
+                    cin >> choice2;
+
+                } while (choice2 != '1');
+                break;
+
+            case '5':
+                cout << "Do zobaczenia!\n";
+                break;
+
+            default:
+                cout << "Niepoprawny wybor\n";
+                break;
+        }
 
     } while (choice != '5');
 
