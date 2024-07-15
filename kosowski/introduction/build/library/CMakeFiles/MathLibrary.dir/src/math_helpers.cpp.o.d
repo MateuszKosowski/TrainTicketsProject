3c748449373120d931030431078bf214e7668960
Mateusz Kosowski <mateuszkosowski03@gmail.com>
2024-07-16 00:33:36 +0200

    Delete kosowski dir

--- kosowski/introduction/build/library/CMakeFiles/MathLibrary.dir/src/math_helpers.cpp.o.d
+++
@@ -1,4 +0,0 @@
-library/CMakeFiles/MathLibrary.dir/src/math_helpers.cpp.o: \
- /home/student/Pulpit/CLionProjects/start/kosowski/introduction/library/src/math_helpers.cpp \
- /usr/include/stdc-predef.h \
- /home/student/Pulpit/CLionProjects/start/kosowski/introduction/library/include/math_helpers.h
